632eb6d98c1cd74070941f892f9fd7389abbd7d4
samilieberman <samilieberman1@gmail.com>
2022-11-06 19:28:41 -0500

    add hello world message
    Add new text and gator image
    
    Replaced Sami with Ian Kreger to put my name in and changed version to 1.0.0
    
    Replaced Sami with Ian Kreger to put my name in and changed version to 1.0.0
    
    add hello world message

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import gatorLogo from './gator.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+<<<<<<< HEAD
+        <img src={gatorLogo} className="App-logo" alt="logo" />
+        <p> Hello World! </p>
+        <p> My name is Ian Kreger and this is my project </p>
+=======
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+>>>>>>> 6cb53b9 (add hello world message)
       </header>
     </div>
   );
